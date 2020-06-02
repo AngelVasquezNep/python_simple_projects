bc1c79cd81bbcb75a20825b24e2eb3a4260ec210
Angelito <angel.vasquez.unam@gmail.com>
2020-06-01 23:02:21 -0500

    Add User module documentation

--- users.py
+++ users.py
@@ -2,10 +2,17 @@ import time
 
 
 class Users:
+    """To manage user.
+    You can create, search, delete or update users.
+    Each user has an incremental id, created_at and deleted_at value
+    """
+
     def __init__(self):
         self.users = []
 
     def register_a_new_user(self):
+        """To add a new user"""
+
         print("User info")
         full_name = input("Fullname: ")
         age = int(input("Age: "))
@@ -26,6 +33,12 @@ class Users:
             print("User {} was added successfully!".format(full_name))
 
     def _print_users(self, users):
+        """To show a list of users
+        
+        Args:
+            users: list of users
+        """
+
         for user in users:
             user_info = "Id: {id} | Full Name: {full_name} | Age: {age} | created_at: {created_at}".format(
                 **user)
@@ -35,7 +48,9 @@ class Users:
             print("*" * len(user_info))
             print('\n')
 
-    def show_all_users(self):
+    def get_all_active_users(self):
+        """To get all active users (without deleted_at value)"""
+
         if len(self.users) == 0:
             print("No users yet")
 
@@ -44,6 +59,14 @@ class Users:
                 [user for user in self.users if user['deleted_at'] is None])
 
     def get_user_by_id(self, user_id):
+        """To get an user by its id
+        
+        Args:
+            user_id: An valid user id
+        Raises:
+            IndexError for non-existent user
+        """
+
         try:
             c = [user for user in self.users if user['id'] == user_id]
 
@@ -52,6 +75,11 @@ class Users:
             return "User {} doesn't exist".format(user_id)
 
     def delete_user(self, user_id):
+        """Delete some user by its id
+        Args:
+            user_id: A valid user id
+        """
+
         user = self.get_user_by_id(user_id)
 
         if 'id' in user:
@@ -71,6 +99,8 @@ class Users:
         """
 
     def run(self):
+        """Use to start the program"""
+
         while True:
             command = input("> ").lower()
 
@@ -79,13 +109,13 @@ class Users:
 
             elif command == "add":
                 self.register_a_new_user()
-            
+
             elif command == "delete":
                 user_id = int(input(">User id "))
                 self.delete_user(user_id)
 
             elif command == "all":
-                self.show_all_users()
+                self.get_all_active_users()
 
             elif command == "get":
                 user_id = int(input(">User id "))
