d0a5d65eed2de28533938e77b3126a9dd59597b5
Angelito <angel.vasquez.unam@gmail.com>
2020-06-01 22:40:25 -0500

    Document iterators_test.py

--- iterators_test.py
+++ iterators_test.py
@@ -1,8 +1,16 @@
-def get():
+def create_an_iterator():
+    """This function can pass as argument in any iterator
+    constructor like list and you'll get an iterator.
+
+    Example:
+      list(create_an_iterator())
+      >>> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    """
     for a in range(10):
         yield a
 
 
-c = list(get())
+if __name__ == "__main__":
+    c = list(create_an_iterator())
 
-print(c)
+    print(c)
